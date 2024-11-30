2204eed55e23916196937ff369d95cce3c5cf20a
Afraz Akram <akrama7@mcmaster.ca>
2024-11-30 00:34:26 -0500

    p1

--- objPos.cpp
+++ objPos.cpp
@@ -19,6 +19,29 @@ objPos::objPos(int xPos, int yPos, char sym)
 // Respect the rule of six / minimum four
 // [TODO] Implement the missing special member functions to meet the minimum four rule
 
+objPos::~objPos(){
+    delete[] pos;
+    pos = nullptr;
+}
+objPos::objPos(const objPos &o){
+    pos->x = o.pos->x;
+    pos->y = o.pos->y;
+    symbol = o.symbol;
+    pos = new Pos;
+    pos = o.pos;
+}
+objPos& objPos::operator=(const objPos &o){
+    if (&o != this){
+		delete[] this->pos;
+		this->pos->x = o.pos->x;
+		this->pos->y = o.pos->y;
+        this->symbol = o.symbol;
+		this->pos = new Pos;
+		this->pos = o.pos;
+	}
+	return *this;
+}
+
 
 
 
